cdb0038bfb0c1212066d7e3cb1b7bfc8cdf13b2d
NAIM 69 <151936671+mrnaim69@users.noreply.github.com>
2024-11-23 14:32:45 +0600

    Update 01_Variable.js

--- 01_Basic/01_Variable.js
+++ 01_Basic/01_Variable.js
@@ -1,8 +1,44 @@
 
 // Variable in JavaScript 
 
-console.log("Hello World !");
+console.log("Hello World!");
 
-const firstProgramme = "Hello World !";
+const firstProgramme = "Hello World!";
 
 console.log(firstProgramme);
+
+
+// Variable ( var , let , const )
+
+//var myName = "Muhammad Naimur";
+
+// var isn't recommend at all
+
+let myName = "Muhmmad Naimur";
+
+console.log(myName);
+
+myName = "Naim";
+
+const myGoal = "Full Stack Web Developer ";
+
+
+console.log("My name is " + myName + ". My goal is to be a " + myGoal);
+
+
+console.log(`My name is ${myName}. My goal is to be a ${myGoal}`);
+
+
+// Template Literals ``
+
+
+// myGoal = "Naim"; it is not possible because we cann't re-assign a const variable 
+
+
+// Recommendation 
+
+// Always use let and const 
+
+// let for simple variable and const for constant variable 
+
+  
