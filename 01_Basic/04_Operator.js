a426144a79b60afcae9425b975884b783d1a4c3b
NAIM 69 <151936671+mrnaim69@users.noreply.github.com>
2024-11-23 18:26:29 +0600

    Update 04_Operator.js

--- 01_Basic/04_Operator.js
+++ 01_Basic/04_Operator.js
@@ -100,3 +100,23 @@ x %= 2 ; // x = x % 2 ;
 
 
 
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
